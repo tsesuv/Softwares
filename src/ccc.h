51289b8d49eb8053689a058ccd28db6c96fc495f
tsesuv notsel <arefoot2@gmail.com>
2025-06-16 16:13:21 +0900

    Create ccc.h
    cccのヘッダファイルです。cccについてはccc.txtを読んでください。

---
+++ src/ccc.h
@@ -0,0 +1,48 @@
+#ifndef CCC_H
+#define CCC_H
+
+////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <termios.h>
+#include <sys/ioctl.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#define BLACK 0
+#define RED 1
+#define GREEN 2
+#define YELLOW 3
+#define BLUE 4
+#define MAGENTA 5
+#define CYAN 6
+#define WHITE 7
+
+////////////////////////////////////////////////////////
+
+int cls(void);
+
+int setmode(int);
+
+int setcclr(int);
+int setbclr(int);
+
+int resetclr(void);
+
+int getrow(void);
+int getcol(void);
+int getposx(void);
+int getposy(void);
+
+int acs(int, int);
+int rcs(int, int);
+
+int bprint(const char *text, ...);
+int bprintln(const char *text, ...);
+
+int kbhit(void);
+int csloff(int);
+
+#endif
