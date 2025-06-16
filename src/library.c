1cf53d670a1e94e19a04c9cfe9553a90768894aa
tsesuv notsel <arefoot2@gmail.com>
2025-06-16 16:33:50 +0900

    Update library.c

--- src/library.c
+++ src/library.c
@@ -3,148 +3,231 @@
 #else
 #define dllexp /*extern __declspec(dllexport)*/
 #endif
+#define TABLE_SIZE (2 < 16)
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
+#include "hash.h"
 
 /* 関数宣言 */
-dllexp char* rewrite(char *p, int i, char d);
-dllexp char* trim (char *p, int l, int i);
-dllexp char* find(char *p, int l, char tgt);
-dllexp char* split(char *p, int l, char dchar, int i);
-dllexp char* slice(char *p, int l, int si, int ei);
-dllexp char* swap(char *p, int l);
-dllexp char* unique(char *p, int l);
+dllexp char *rewrite(char *p, int i, char d);
+dllexp char *trim(char *p, int i);
+dllexp char *find(char *p, char tgt);
+dllexp char *split(char *p, char dchar, int i);
+dllexp char *slice(char *p, int si, int ei);
+dllexp char *swap(char *p);
+dllexp char *unique(char *p);
 dllexp void sort(int *p, int l);
 dllexp void twss(int *p, int l);
-dllexp int lhash(char *p, int l);
-dllexp uint8_t xor(uint8_t in1, uint8_t in2);
-dllexp uint8_t xed_enc(uint8_t p);
-dllexp uint8_t xed_dec(uint8_t p);
-dllexp unsigned int dec2bin(unsigned int p);
-dllexp void msgtype(int tablenum);
+dllexp unsigned long long dec2bin(unsigned long long p);
+dllexp unsigned long long bin2dec(unsigned long long p);
+dllexp unsigned long long *dip(unsigned long long p);
+dllexp int SetVarData(const char *VarName, const char *VarData);
+dllexp void *GetVarData(const char *VarName);
+dllexp void tag(int tablenum);
 /* 2024/12/22/12:26:関数宣言部を作成。 */
 
-char res_list[0xFFFF] = "";; /* 2024/12/20/15:55:引数で変数を指定する方式から
-					   関数の返り値を使用する方式に変更するため、
-					   変数をグローバルとして宣言。65535文字までなら何とか。 */
+// char res_list[0xFFFF] = "";
+/* 2024/12/20/15:55:引数で変数を指定する方式から
+関数の返り値を使用する方式に変更するため、
+変数をグローバルとして宣言。65535文字までなら何とか。 */
+/* 配列を使用するすべての関数において動的メモリ化が完了したため、各関数内でres_listを宣言する形に戻しました。 */
 
-dllexp char* rewrite(char *p, int i, char d)
+typedef struct
+{
+	char *name;
+	void *value;
+	struct VarTable *next;
+} VarTable;
+
+VarTable *HashTable[TABLE_SIZE];
+
+dllexp char *rewrite(char *p, int i, char d)
 {
 	/* 2024/12/07/12:49:入力配列pを破壊しないように変更しました */
-	
+
 	int memsize = 1;
-	char* nres_list = (char*)malloc(memsize); /* 2025/01/13/07:56:試験的に動的メモリを使用するようにしました。 */
-	if(!nres_list)
+	char *res_list = (char *)malloc(memsize); /* 2025/01/13/07:56:試験的に動的メモリを使用するようにしました。 */
+	if (!res_list)
 	{
-		free(nres_list);
+		free(res_list);
 		return NULL;
 	}
 	int k = 0;
-	
-	while(*(p + k) != '\0')
-	{
-		*(nres_list + k) = *(p + k); /* 2024/12/19/09:40:バグがあったので修正 */
-		if(k == i)
-		{
-			*(nres_list + k) = d;
-		}
-		k++;
 
-		if(memsize <= k)
+	while (*(p + k) != '\0')
+	{
+		if (memsize <= k)
 		{
 			memsize++;
-			char* tmp = (char*)realloc(nres_list, memsize * sizeof(char*));
-			if(!tmp)
+			char *tmp = (char *)realloc(res_list, memsize * sizeof(char *));
+			if (!tmp)
 			{
-				free(nres_list);
+				free(res_list);
 				free(tmp);
 				return NULL;
 			}
-			
-			nres_list = tmp;
+
+			res_list = tmp;
 		}
+
+		*(res_list + k) = *(p + k); /* 2024/12/19/09:40:バグがあったので修正 */
+		if (k == i)
+		{
+			*(res_list + k) = d;
+		}
+		k++;
 	}
-	*(nres_list + k) = '\0';
-	return nres_list;
+	*(res_list + k) = '\0';
+	return res_list;
+	free(res_list);
 }
 
-dllexp char* trim(char *p, int l, int i)
+dllexp char *trim(char *p, int i)
 {
-	/* 2024/12/07/12:49:入力配列pを破壊しないように変更しました */ 
-	
-	for (int j = 0; j < l; j++)
+	/* 2024/12/07/12:49:入力配列pを破壊しないように変更しました */
+	int memsize = 1;
+	char *res_list = (char *)malloc(memsize);
+	if (!res_list)
 	{
-		if(i <= j)
+		free(res_list);
+		return NULL;
+	}
+	int k = 0;
+	int l = 0;
+
+	while (*(p + l) != '\0')
+	{
+		l++;
+	}
+
+	while (*(p + k) != '\0')
+	{
+		if (memsize <= k)
 		{
-			if (j == l - 1)
+			memsize++;
+			char *tmp = (char *)realloc(res_list, memsize * sizeof(char *));
+			if (!tmp)
 			{
-				res_list[j] = '\0';
+				free(res_list);
+				free(tmp);
+				return NULL;
+			}
+			res_list = tmp;
+		}
+
+		if (i <= k)
+		{
+			if (k == l - 1)
+			{
+				*(res_list + k) = '\0';
 			}
 			else
 			{
-				res_list[j] = *(p + j + 1);
+				*(res_list + k) = *(p + k + 1);
 			}
 		}
 		else
 		{
-			res_list[j] = *(p + j);
+			*(res_list + k) = *(p + k);
 		}
 	}
 	return res_list;
 }
 
-dllexp char* find(char *p, int l, char tgt)
+dllexp char *find(char *p, char tgt)
 {
-	int j = 0;
-	for(int i = 0; i < l; i++)
+	int memsize = 1;
+	char *res_list = (char *)malloc(memsize);
+	if (!res_list)
 	{
-		if(*(p + i) == tgt)
+		free(res_list);
+		return NULL;
+	}
+	int k = 0;
+	int i = 0;
+
+	while (*(p + k) != '\0')
+	{
+		if (memsize <= k)
 		{
-			res_list[j] = i;
-			j++;
+			memsize++;
+			char *tmp = (char *)realloc(res_list, memsize * sizeof(char *));
+			if (!tmp)
+			{
+				free(res_list);
+				free(tmp);
+				return NULL;
+			}
+			res_list = tmp;
 		}
+
+		if (*(p + k) == tgt)
+		{
+			*(res_list + i) = k;
+			i++;
+		}
+		k++;
 	}
 	return res_list;
 }
 
 /* 2024/12/06/21:24:old_splitは削除されました */
 
-dllexp char* split(char *p, int l, char dchar, int i)
+dllexp char *split(char *p, char dchar, int i)
 {
+	/* 2025/01/19/17:55:遂にsplitで動的メモリ化。splitは独特な動きをするからめんどかった・・・ */
+
 	int k = 0;
 	int n = 0;
-	 
+	int l = 0;
+	int memsize = 0;
+	while(*(p + l) != '\0')
+	{
+		l++;
+		memsize++;
+	}
+	char *res_list = (char *)malloc(memsize - 1);
+	if(!res_list)
+	{
+		free(res_list);
+		return NULL;
+	}
+	for(int m = 0; m < l; m++)
+	{
+		*(res_list + m) = 0;
+	}
 	char tmp[l + 2];
 
-	for(int ti = 0; ti < l; ti++)
+	for (int ti = 0; ti < l; ti++)
 	{
-		tmp[ti + 1] = *(p + ti);
+		*(tmp + ti + 1) = *(p + ti);
 	}
-	tmp[l + 1] = dchar;
-	tmp[0] = dchar;
+	*(tmp + l + 1) = dchar;
+	*tmp = dchar;
 
-	for(int j = 0; j < l + 2; j++)
+	for (int j = 0; j < l + 2; j++)
 	{
-		if(tmp[j] == dchar)
+		if (*(tmp + j) == dchar)
 		{
 			k++;
-			if(k - 1 == i)
+			if (k - 1 == i)
 			{
 				j++;
-				while(1)
+				while (1)
 				{
-					if(k - 1 == i + 1)
+					if (k - 1 == i + 1)
 					{
 						return res_list;
 					}
-					if(tmp[j] == dchar)
+					if (*(tmp + j) == dchar)
 					{
 						k++;
 					}
 					else
 					{
-						res_list[n] = tmp[j];
+						*(res_list + n) = *(tmp + j);
 					}
 					n++;
 					j++;
@@ -155,49 +238,71 @@ dllexp char* split(char *p, int l, char dchar, int i)
 	return res_list;
 }
 
-dllexp char* slice(char *p, int l, int si, int ei)
+dllexp char *slice(char *p, int si, int ei)
 {
+	int l = 0;
+	while(*(p + l) != '\0')
+	{
+		l++;
+	}
 	char tmp[l + 2];
 
-	for(int i = 0; i < l; i++)
+	for (int i = 0; i < l; i++)
 	{
-		tmp[i + 1] = *(p + i);
+		*(tmp + i + 1) = *(p + i);
 	}
-	tmp[ei + 1] = '\0';
-	tmp[si] = '\0';
-	return split(&tmp[0], l, '\0', 1);
+	*(tmp + ei + 1) = 0x01;
+	*(tmp + si) = 0x01;
+	return split(tmp, 0x01, 1);
 }
 
-dllexp char* swap(char *p, int l)
+dllexp char *swap(char *p)
 {
-	for(int i = 0; i < l; i++)
+	int l = 0;
+	int memsize = 0;
+	while(*(p + l) != '\0')
 	{
-		res_list[i] = *(p + l - i - 1);
+		l++;
+		memsize++;
+	}
+	char *res_list = (char *)malloc(memsize - 1);
+
+	for (int i = 0; i < l; i++)
+	{
+		*(res_list + i) = *(p + l - i - 1);
 	}
 	return res_list;
 }
 
-dllexp char* unique(char *p, int l)
+dllexp char *unique(char *p)
 {
 	int k = 0;
+	int l = 0;
+	int memsize = 0;
+	while(*(p + l) != '\0')
+	{
+		l++;
+		memsize++;
+	}
+	char *res_list = (char *)malloc(memsize - 1);
 
-	for(int i = 0; i < l; i++)
+	for (int i = 0; i < l; i++)
 	{
 		/* 2024/12/06/21:21:修正完了。もともとのコードは消しました。 */
 		/* n番目の要素と*(p + i)が一致したらflagを1にする、forする前に0にして置き、forし終わった後にflag==0ならres_list[k]に追加、k++をする */
-	   int flag = 0;
-		for(int n = 0; n < l; n++)
+		int flag = 0;
+		for (int n = 0; n < l; n++)
 		{
-			if(*(p + i) == res_list[n])
+			if (*(p + i) == *(res_list + n))
 			{
 				flag = 1;
 				break; // AIの指摘により、「flagを立てたら、そのあとは調べるの無意味じゃない？」ということでforから抜けるように。
 			}
 		}
 
-		if(flag == 0)
+		if (flag == 0)
 		{
-			res_list[k] = *(p + i);
+			*(res_list + k) = *(p + i);
 			k++;
 		}
 	}
@@ -217,12 +322,14 @@ dllexp void twss(int *p, int l)
 	なお、既存のプログラムでの利用でエラーが起きないよう、
 	sort関数ではtwssを呼び出すよう変更しております。 */
 	int tmp; // 値のコピー用
+	/* 2025/01/26/14:21:やっぱりtwssに関しては長さは明示的に指定するほうがいい気がするので戻してみました。 */
 	int i = 0;
 	int k = l - 1;
 
-	while(1)
+	/* 2025/01/26/14:26:AIの指摘により、whileループの条件のほうでループを抜けるようにしました。 */
+	while (i < l - 1)
 	{
-		if(*(p + i) > *(p + k))
+		if (*(p + i) > *(p + k))
 		{
 			tmp = *(p + i);
 			*(p + i) = *(p + k);
@@ -230,119 +337,33 @@ dllexp void twss(int *p, int l)
 		}
 		k--;
 
-		if(i == k)
+		if (i == k)
 		{
 			i++;
 			k = l - 1;
 		}
-		if(i == l - 1)
-		{
-			return;
-		}
 	}
+	return;
 }
 
-dllexp int lhash(char *p, int l)
-{
-	/* 2024/12/19/21:18:実装。lはlikeのl。 */
-	int res = 0;
-	int i = 0;
-	int tmp = 0;
-
-	for(i = 0; i < l; i++)
-	{
-		tmp = res % ((int)*(p + i) * (i + 1) - tmp) + (int)*(p + i);  /* 2024/12/22/15:26:テストした結果順序が異なっても同じ値になってしまっていたため修正。 */
-		res = res + tmp;
-	}
-	return res;
-}
-
-/* 2025/01/13/08:15:以下がXEA、正式名称をXor Encode/Decode Algorithmのエンコード・デコードアルゴリズム。 */
-/*
-エンコード：
-①インデックスiを0に初期化
-②入力バイナリはin配列で表現(in[0]は最上位ビット)
-③出力バイナリはout配列で表現(out[0]は最上位ビット)
-④in[0]をout[0]にロード
-⑤in[i]とin[i+1]のXORをout[i]にロード
-⑥iをインクリメント
-⑦i<(inのサイズ)であれば⑤に飛ぶ
-デコード：
-①インデックスiを1に初期化
-②入力バイナリはin配列で表現(in[0]は最上位ビット)
-③出力バイナリはout配列で表現(out[0]は最上位ビット)
-④in[0]をout[0]にロード
-⑤in[i]が1ならば、in[i-1]をNOTしてout[i]にロード
-⑥in[i]が1でなければ、in[i-1]をout[i]にロード
-⑦iをインクリメント
-⑧i<(inのサイズ)であれば⑤に飛ぶ
-*/
-
-/* 2025/01/13/14:27:実装。シフトする回数を調整するのがめんどかったため試験的に8,8(8Bit入力8Bit出力)でのみサポート。 */
-
-uint8_t xor(uint8_t in1, uint8_t in2)
-{
-	int tmp = 0;
-	for(int i = 8; 0 <= i; i--)
-	{
-		tmp += (((in1 >> i) % 2) || ((in2 >> i) % 2)) - (((in1 >> i) % 2) && ((in2 >> i) % 2));
-		if(i != 0)
-		tmp <<= 1;
-	}
-	return tmp;
-}
-
-dllexp uint8_t xea(uint8_t p)
-{
-	uint8_t result = 0;
-
-	result = ((p >> 7) % 2) << 7;
-
-	for(int i = 6; 0 <= i; i--)
-	{
-		result += xor((p >> (i + 1)) % 2, (p >> i) % 2) << i;
-	}
-	return result;
-}
-
-dllexp uint8_t xda(uint8_t p)
-{
-	uint8_t result = 0;
-
-	result = ((p >> 7) % 2);
-	result <<= 1;
-
-	for(int i = 6; 0 <= i; i--)
-	{
-		if(!((p >> i) % 2))
-		result += (result >> 1) % 2;
-		else
-		result += ((result >> 1) + 1) % 2;
-		
-		if(0 < i)
-		result <<= 1;
-	}
-
-	return result;
-}
-
-dllexp unsigned int dec2bin(unsigned int p)
+dllexp unsigned long long dec2bin(unsigned long long p)
 {
 	/* 2025/01/13/12:10:実装。入力pに対して、二進表記の結果を10進数で返す。 */
 	/* 見かけ上は2進数だが、実際は10進数として認識されることに注意。 */
 	/* また変換可能な値の範囲は0-1023であることにも注意。 */
-	unsigned int tmp = p;
-	int sbr = 1;
-	int counter = 0;
-	unsigned int result = 0;
+	/* 2025/02/13/19:37:0-1048575に拡張。 */
+	unsigned long long tmp = p;
+	unsigned int sbr = 1;
+	unsigned int counter = 0;
+	unsigned long long result = 0;
 
-	while(p != 0)
+	while (p != 0)
 	{
 		tmp = p;
 		sbr = 1;
 		counter = 0;
 
-		while(tmp != 1)
+		while (tmp != 1)
 		{
 			tmp >>= 1;
 			sbr <<= 1;
@@ -351,9 +372,9 @@ dllexp unsigned int dec2bin(unsigned int p)
 		p -= sbr;
 		tmp = 1;
 
-		for(int i = 0; i < counter; i++)
+		for (int i = 0; i < counter; i++)
 		{
-			tmp = tmp * 10;
+			tmp *= 10;
 		}
 		result += tmp;
 	}
@@ -361,7 +382,128 @@ dllexp unsigned int dec2bin(unsigned int p)
 	return result;
 }
 
-dllexp void msgtype(int tablenum)
+dllexp unsigned long long bin2dec(unsigned long long p)
+{
+	/* 2025/02/13/19:10:実装。'dec2bin`による見かけ上の2進数を10進数に変換。 */
+	/* 2025/02/13/19:37:0-1048575に拡張。 */
+	unsigned long long result = 0;
+	unsigned int k = 0;
+	while(p != 0)
+	{
+		if((p % 10) == 1)
+		{
+			result += (1 << k);
+		}
+		p /= 10;
+		k++;
+	}
+	return result;
+}
+
+dllexp uint8_t chr2int(const char *p)
+{
+	int result = 0;
+	int i = 0;
+	while(*(p + i) != 0)
+	{
+		result *= 10;
+		result += (int)*(p + i) - 0x30;
+		i++;
+	}
+	return result;
+}
+
+dllexp unsigned long long *dip(unsigned long long p)
+{
+	/*2025/01/11/21:01:実装。8Byte整数を素因数分解する。*/
+
+	unsigned long long k = 0;
+	unsigned long long i = 2;
+	unsigned long long memsize = 1;
+	unsigned long long *result = (unsigned long long *)malloc(memsize * sizeof(unsigned long long));
+	if(!result)
+	{
+		free(result);
+		return NULL;
+	}
+
+	if(p == 1)
+	{
+		*result = 1;
+		return result;
+	}
+	else if(p == 0)
+	{
+		return NULL;
+	}
+
+	while(p != 1)
+	{
+		if(memsize <= k)
+		{
+			memsize++;
+			unsigned long long *tmp = (unsigned long long *)realloc(result, memsize * sizeof(unsigned long long));
+			if(!tmp)
+			{
+				free(tmp);
+				free(result);
+				return NULL;
+			}
+			result = tmp;
+		}
+
+		if((p % i) == 0)
+		{
+			*(result + k) = i;
+			p /= i;
+			k++;
+			i--;
+		}
+		i++;
+	}
+	return result;
+}
+
+dllexp int SetVarData(const char *VarName, const char *VarData)
+{
+	/* 2025/03/11/12:02:実装。動的型付けの動的変数。グローバルのみ。 */
+
+	uint32_t index = fnvla(VarName) % TABLE_SIZE;
+	VarTable *OldVar = HashTable[index];
+	while(OldVar != NULL)
+	{
+		if(strcmp(OldVar->name, VarName) == 0)
+		{
+			OldVar->value = (void *)VarData;
+			return 0;
+		}
+		OldVar = (VarTable *)OldVar->next;
+	}
+
+	VarTable *Var = (VarTable *)malloc(sizeof(VarTable));
+	Var->name = (char *)VarName;
+	Var->value = (void *)VarData;
+	Var->next = (struct VarTable *)HashTable[index];
+	HashTable[index] = Var;
+	return 0;
+}
+
+dllexp void *GetVarData(const char *VarName)
+{
+	uint32_t index = fnvla(VarName) % TABLE_SIZE;
+	VarTable *Var = HashTable[index];
+	while(Var != NULL)
+	{
+		if(strcmp(Var->name, VarName) == 0)
+		{
+			return Var->value;
+		}
+		Var = (VarTable *)Var->next;
+	}
+	return NULL;
+}
+
+dllexp void tag(int tablenum)
 {
 	switch (tablenum)
 	{
